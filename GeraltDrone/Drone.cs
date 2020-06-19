d2edcfbdfd85a2661479975da1859e2291ecf69b
Visahavel <wizvishak@gmail.com>
2020-06-19 20:30:37 +0530

    hit a stone once, yeay!

--- GeraltDrone/Drone.cs
+++ GeraltDrone/Drone.cs
@@ -20,6 +20,7 @@ namespace GeraltDrone
 		** Fields
 		*********/
 		private readonly IModHelper Helper;
+		private readonly IMonitor Monitor;
 		private float T;
 		private Monster Target;
 		private int Damage;
@@ -57,7 +58,8 @@ namespace GeraltDrone
 			int speed,
 			int damage,
 			float projectileVelocity,
-			IModHelper helper
+			IModHelper helper,
+			IMonitor monitor
 		) : base(
 			new AnimatedSprite(
 				"Sidekick/Drone",
@@ -75,6 +77,7 @@ namespace GeraltDrone
 			this.Damage = damage;
 			this.ProjectileVelocity = projectileVelocity;
 			this.Helper = helper;
+			this.Monitor = monitor;
 
 			this.HakiPower = damage;
 		}
@@ -92,13 +95,17 @@ namespace GeraltDrone
 
 		public override void update(GameTime time, GameLocation location)
 		{
+			// make drone fly around the player
 			float newX = Game1.player.position.X + OFFSET_X + R * (float)Math.Cos(T * 2 * Math.PI);
 			float newY = Game1.player.position.Y - OFFSET_Y + R * (float)Math.Sin(T * 2 * Math.PI);
 
 			this.position.Set(new Vector2(newX, newY));
 			T = (T + (float)time.ElapsedGameTime.TotalMilliseconds / (1000 * speed)) % 1;
 
+			// attack monsters in vicinity
 			this.ProtectPlayer(time, location);
+
+			// break rocks to obtain items
 			this.MineStones(time, location);
 		}
 
@@ -114,10 +121,15 @@ namespace GeraltDrone
 		{
 			if (!this.Destroying)
 			{
-				foreach (KeyValuePair<Vector2, StarObject> pair in location.objects.Pairs)
+				foreach (KeyValuePair<Vector2, StarObject> pair in Game1.currentLocation.objects.Pairs)
 				{
 					// start by shooting and breaking stones; later breakable containers
-					if(pair.Value?.name != STONE) continue;
+
+					// check if stone is in same tile as drone
+					if(
+						pair.Value?.name != STONE
+					   || this.getTileLocation() != pair.Value.TileLocation
+					) continue;
 
 					this.Destroying = true;
 					this.TileTarget = (StarObject) pair.Value;
@@ -125,6 +137,7 @@ namespace GeraltDrone
 					break;
 				}
 
+				// shoot the object on the tile
 				if (this.Destroying && this.TileTarget.name == STONE)
 					this.ShootTheTileObject(time, location, this.TileTarget);
 			}
@@ -154,7 +167,7 @@ namespace GeraltDrone
 				this.Destroyed = true;
 			}
 
-			this.ResetActionProps();
+			this.ResetMiningProps();
 		}
 
 		private CollisionBehavior StoneCollisionBehaviour(StarObject tileObject)
@@ -167,11 +180,29 @@ namespace GeraltDrone
 					if (Game1.player.CurrentTool != null && Game1.player.CurrentTool is Tool)
 						currentTool = Game1.player.CurrentTool;
 
+					this.Monitor.Log($"Trying to break stone at {tileObject.TileLocation}", LogLevel.Info);
 					// collision checks and logic
-					loc.destroyObject(
-						tileObject.TileLocation,
-						!(who is Farmer) ? Game1.player : who as Farmer
-					);
+					Tool iridiumPickaxe = this.GetIridiumPickaxe();
+					tileObject.performToolAction(iridiumPickaxe, loc);
+
+					// Approach #3: Hit the stone with the pickaxe once
+					//Tool pickaxe = new Pickaxe();
+					//this.UseToolOnTile(
+					//	pickaxe,
+					//	tileObject.TileLocation,
+					//	!(who is Farmer) ? Game1.player : who as Farmer,
+					//	loc
+					//);
+
+					// Approach #2: Perform tool action
+					//Tool pickaxe = new Pickaxe();
+					//loc.performToolAction(pickaxe, (int)tileObject.TileLocation.X, (int)tileObject.TileLocation.Y);
+
+					// Approach #1: Destroy the object
+					//loc.destroyObject(
+					//	tileObject.TileLocation,
+					//	!(who is Farmer) ? Game1.player : who as Farmer
+					//);
 
 					if (Game1.player.CurrentTool != null
 					    && Game1.player.CurrentTool is Tool
@@ -181,7 +212,38 @@ namespace GeraltDrone
 			);
 		}
 
-		private void ResetActionProps()
+		private Tool GetIridiumPickaxe()
+		{
+			Tool pickaxe = new Pickaxe();
+			this.Helper.Reflection
+				.GetField<NetInt>(pickaxe, "upgradeLevel")
+				.SetValue(new NetInt(4));
+
+			return pickaxe;
+		}
+
+		/// <summary>Use a tool on a tile.</summary>
+		/// <param name="tool">The tool to use.</param>
+		/// <param name="tile">The tile to affect.</param>
+		/// <param name="player">The current player.</param>
+		/// <param name="location">The current location.</param>
+		/// <returns>Returns <c>true</c> for convenience when implementing tools.</returns>
+		protected bool UseToolOnTile(Tool tool, Vector2 tile, Farmer player, GameLocation location)
+		{
+			// use tool on center of tile
+			player.lastClick = this.GetToolPixelPosition(tile);
+			tool.DoFunction(location, (int)player.lastClick.X, (int)player.lastClick.Y, 0, player);
+			return true;
+		}
+
+		/// <summary>Get the pixel position relative to the top-left corner of the map at which to use a tool.</summary>
+		/// <param name="tile">The tile to affect.</param>
+		protected Vector2 GetToolPixelPosition(Vector2 tile)
+		{
+			return (tile * Game1.tileSize) + new Vector2(Game1.tileSize / 2f);
+		}
+
+		private void ResetMiningProps()
 		{
 			if (this.Destroyed && this.CannonBall is BasicProjectile && this.CannonBall.destroyMe)
 			{
@@ -218,49 +280,7 @@ namespace GeraltDrone
 				if (this.Damage == -1)
 					this.Damage = monster.Health;
 
-				var collisionBehavior = new CollisionBehavior(
-					delegate (GameLocation loc, int x, int y, Character who)
-					{
-						Tool currentTool = null;
-
-						if (Game1.player.CurrentTool != null && Game1.player.CurrentTool is Tool)
-							currentTool = Game1.player.CurrentTool;
-
-						if (monster is Bug bug && bug.isArmoredBug)
-							this.Helper.Reflection
-								.GetField<NetBool>(bug, "isArmoredBug")
-								.SetValue(new NetBool(false));
-
-						if (monster is RockCrab rockCrab)
-						{
-							if (Game1.player.CurrentTool != null
-							    && Game1.player.CurrentTool is Tool
-							    && currentTool != null
-							    && Game1.player.CurrentTool is Pickaxe)
-								Game1.player.CurrentTool = new MeleeWeapon(4);
-
-							this.Helper.Reflection
-								.GetField<NetBool>(rockCrab, "shellGone")
-								.SetValue(new NetBool(true));
-							this.Helper.Reflection
-								.GetField<NetInt>(rockCrab, "shellHealth")
-								.SetValue(new NetInt(0));
-						}
-
-						loc.damageMonster(
-							monster.GetBoundingBox(),
-							this.Damage,
-							this.Damage + 1,
-							true,
-							!(who is Farmer) ? Game1.player : who as Farmer
-						);
-
-						if (Game1.player.CurrentTool != null
-						    && Game1.player.CurrentTool is Tool
-						    && currentTool != null)
-							Game1.player.CurrentTool = currentTool;
-					}
-				);
+				var collisionBehavior = this.MonsterCollisionBehaviour(monster);
 				Vector2 velocityTowardMonster = this.GetVelocityTowardMonster(monster);
 
 				this.CannonBall = this.GetCannonBall(
@@ -274,6 +294,53 @@ namespace GeraltDrone
 			}
 
 			this.ResetAttackProps();
+		}
+
+		private CollisionBehavior MonsterCollisionBehaviour(Monster monster)
+		{
+			return new CollisionBehavior(
+				delegate(GameLocation loc, int x, int y, Character who)
+				{
+					Tool currentTool = null;
+
+					if (Game1.player.CurrentTool != null && Game1.player.CurrentTool is Tool)
+						currentTool = Game1.player.CurrentTool;
+
+					if (monster is Bug bug && bug.isArmoredBug)
+						this.Helper.Reflection
+							.GetField<NetBool>(bug, "isArmoredBug")
+							.SetValue(new NetBool(false));
+
+					if (monster is RockCrab rockCrab)
+					{
+						if (Game1.player.CurrentTool != null
+						    && Game1.player.CurrentTool is Tool
+						    && currentTool != null
+						    && Game1.player.CurrentTool is Pickaxe)
+							Game1.player.CurrentTool = new MeleeWeapon(4);
+
+						this.Helper.Reflection
+							.GetField<NetBool>(rockCrab, "shellGone")
+							.SetValue(new NetBool(true));
+						this.Helper.Reflection
+							.GetField<NetInt>(rockCrab, "shellHealth")
+							.SetValue(new NetInt(0));
+					}
+
+					loc.damageMonster(
+						monster.GetBoundingBox(),
+						this.Damage,
+						this.Damage + 1,
+						true,
+						!(who is Farmer) ? Game1.player : who as Farmer
+					);
+
+					if (Game1.player.CurrentTool != null
+					    && Game1.player.CurrentTool is Tool
+					    && currentTool != null)
+						Game1.player.CurrentTool = currentTool;
+				}
+			);
 		}
 
 		private Vector2 GetVelocityTowardMonster(Monster monster)
